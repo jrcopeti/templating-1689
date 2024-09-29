c6a0db57b28d8f5ed5ec7faced25898fae493b19
Jose Copeti <jrcopeti@gmail.com>
2024-09-29 14:47:08 +0200

    initial commit

---
+++ javascript/async-await.js
@@ -0,0 +1,12 @@
+// async function fetchMovies() {
+//   try {
+//     const response = await fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7");
+//     const data = await response.json();
+//     console.log(data);
+//     // do something with the data
+//   } catch (error) {
+//     console.error("Error fetching movies:", error);
+//   }
+// }
+
+// fetchMovies();
